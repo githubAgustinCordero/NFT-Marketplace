2c70edafc3401b97a9464a01b45ea6fe8627184b
Agustin Cordero <agustincordero@gmail.com>
2022-12-22 19:44:23 +0100

    init

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  contracts_build_directory: './client/src/contracts',
+
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    bsctestnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+      },
+  },
+
+  compilers: {
+    solc: {
+      version: "^0.8.7",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  }
+};
